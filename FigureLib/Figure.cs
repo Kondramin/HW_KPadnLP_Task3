4201cdef3f450f8debe7a5cc68bb551a0dac7fb0
Артём Кондратьев <exitway12@gmail.com>
2020-05-01 17:42:54 +0300

    Added class "Triangle" with base logic.

--- FigureLib/Figure.cs
+++ FigureLib/Figure.cs
@@ -2,7 +2,59 @@
 
 namespace FigureLib
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public abstract class Figure
     {
+        string name;
+        Top a, b, c, d;
+        
+
+        public Figure(Top a, Top b, Top c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public Figure(Top a, Top b, Top c, Top d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+
+
+        public string Name { get => name; set => name = value; }
+        public Top A { get => a; set => a = value; }
+        public Top B { get => b; set => b = value; }
+        public Top C { get => c; set => c = value; }
+        public Top D { get => d; set => d = value; }
+
+
+
+
+        /// <summary>
+        /// Calculate square of figure.
+        /// </summary>
+        /// <returns>square</returns>
+        public abstract double Square();
     }
+    public struct Top
+    {
+        int x, y;
+
+        public Top(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+
+    }
+
 }
---
+++ FigureLib/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FigureLib
+{
+    class Triangle : Figure
+    {
+        public Triangle(Top a, Top b, Top c) : base(a, b, c)
+        {
+            Name = "Triangle";
+        }
+
+        public override double Square()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
