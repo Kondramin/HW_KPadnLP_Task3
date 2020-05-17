256ec400e4f07491331407ef328cb80004e9f18a
Артём Кондратьев <exitway12@gmail.com>
2020-05-18 00:23:11 +0300

    Modify WPF

--- WpfInterface/MainWindow.xaml.cs
+++ WpfInterface/MainWindow.xaml.cs
@@ -23,11 +23,19 @@ namespace WpfInterface
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void ButClear_Click(object sender, RoutedEventArgs e)
         {
             TBInfo.Text = "";
+        }
+
+        private void ButAddFig_Click(object sender, RoutedEventArgs e)
+        {
+            CreateFigure createFigure = new CreateFigure();
+            createFigure.Owner = this;
+            createFigure.Show();
         }
     }
 }
